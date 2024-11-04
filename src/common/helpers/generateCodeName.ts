5ae8af00c2097d254c3665df1e8d69562ab665cb
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-11-04 16:12:45 +0100

    fixing code generation

--- src/common/helpers/generateCodeName.ts
+++ src/common/helpers/generateCodeName.ts
@@ -4,17 +4,18 @@ export async function generateCodeName(model: any, repo: any, prefix = 'ORD') {
   const lastModel = await repo.find({
     where: { application: { id: model.application.id } },
     order: { id: 'DESC' },
-    take: 1,
+    // take: 1,
   });
   let nextNumber = 1;
   if (lastModel.length > 0) {
     // Extract the number part from the last `orderCode`
-    const lastModelCode = lastModel[0].ref;
-    if (lastModelCode) {
-      const lastNumber = parseInt(lastModelCode?.replace(`${prefix}-`, ''), 10);
-      // Increment the number
-      nextNumber = lastNumber + 1;
-    }
+    // const lastModelCode = lastModel[0].ref;
+    // if (lastModelCode) {
+    //   const lastNumber = parseInt(lastModelCode?.replace(`${prefix}-`, ''), 10);
+    //   // Increment the number
+    //   nextNumber = lastNumber + 1;
+    // }
+    nextNumber = lastModel.length + 1;
   }
   // Assign the new order code
   model.ref = `${prefix}-${nextNumber}`;
